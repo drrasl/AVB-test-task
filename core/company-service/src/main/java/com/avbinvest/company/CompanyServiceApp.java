3f31c06a6334c8f392915a1c37e3048c622f7834
Ruslan S. <ruslan.drrasl@yandex.ru>
2026-03-07 14:46:26 +0300

    Rev.6 - Some corrections added for proper start

--- core/company-service/src/main/java/com/avbinvest/company/CompanyServiceApp.java
+++ core/company-service/src/main/java/com/avbinvest/company/CompanyServiceApp.java
@@ -3,9 +3,11 @@ package com.avbinvest.company;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableFeignClients(basePackages = "com.avbinvest.company.client")
 public class CompanyServiceApp {
     public static void main(String[] args) {
         SpringApplication.run(CompanyServiceApp.class, args);
